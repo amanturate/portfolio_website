472ed2379d64d46de83a281c27333d28d38b0e50
aturate <turateaman1995@gmail.com>
2019-01-10 04:20:10 +0000

    added

--- jobs/views.py
+++ jobs/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
+from django.db import connection
 from .models import *
 # Create your views here.
+def dictfetchall(cursor): 
+    "Returns all rows from a cursor as a dict" 
+    desc = cursor.description 
+    return [
+            dict(zip([col[0] for col in desc], row)) 
+            for row in cursor.fetchall() 
+    ]
+
 def home(request):
     jobs = Job.objects
     summary = Summary.objects
@@ -8,7 +17,15 @@ def home(request):
     project = Project.objects
     skills = Skill.objects
     awards = Awards.objects
-    return render(request, 'jobs/home.html',{'jobs':jobs, 'summary':summary, 'exp' : experience,'proj' : project, 'skill':skills,'awards':awards})
+    cursor = connection.cursor()
+    try:
+        cursor.execute('''SELECT distinct id,cast(DATE_PART('year',jobs_awards.date) as integer) as yr, name  from jobs_awards''')
+    finally:
+        result = dictfetchall(cursor)
+        cursor.close()
+    print(result)
+    
+    return render(request, 'jobs/home.html',{'jobs':jobs, 'summary':summary, 'exp' : experience,'proj' : project, 'skill':skills,'awd_lst':result,'awards':awards})
 
 def home_2(request):
     jobs = Job.objects
